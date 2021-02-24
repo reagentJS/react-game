606370eb95904b94fe78a42a2b5f75f46763efd2
reagentjs <reagentjs@gmail.com>
2021-02-24 17:57:18 +0800

    feat: finish implementing reveal function

--- src/utils/revealEmptyCells.js
+++ src/utils/revealEmptyCells.js
@@ -1,104 +1,54 @@
 import SIZES from '../constants/SIZES';
 
 export default function revealEmptyCells(grid, id) {
-  grid[id].isRevealed = true;
-
   const flipped = [];
   flipped.push(grid[id]);
 
   while (flipped.length) {
-    console.log(`flipped.length = ${flipped.length}`);
-
     const checkingCell = flipped.pop();
-    const { x, y } = { checkingCell };
+    const { id, x, y } = checkingCell;
+    checkingCell.isRevealed = true;
 
     if (checkingCell.value) {
       break;
     }
 
     if (x > 0) {
-      if (!grid[id - 1].value && !grid[id - 1].isRevealed) {
-        flipped.push(grid[id - 1]);
-        grid[id - 1].isRevealed = true;
-      }
-    }
-
-    if (x < SIZES.cols) {
-      if (!grid[id + 1].value && !grid[id + 1].isRevealed) {
-        flipped.push(grid[id + 1]);
-        grid[id + 1].isRevealed = true;
-      }
-    }
-
-    if (y > 0) {
-      if (!grid[id - SIZES.cols].value && !grid[id - SIZES.cols].isRevealed) {
-        flipped.push(grid[id - SIZES.cols]);
-        grid[id - SIZES.cols].isRevealed = true;
-      }
-    }
-
-    if (y < SIZES.rows) {
-      if (!grid[id + SIZES.cols].value && !grid[id + SIZES.cols].isRevealed) {
-        flipped.push(grid[id + SIZES.cols]);
-        grid[id + SIZES.cols].isRevealed = true;
-      }
-    }
-
-    // diagonals
-    if (x > 0 && y > 0) {
-      if (!grid[id - 1 - SIZES.cols].value && !grid[id - 1 - SIZES.cols].isRevealed) {
-        flipped.push(grid[id - 1 - SIZES.cols]);
-        grid[id - 1 - SIZES.cols].isRevealed = true;
-      }
-    }
-
-    if (x > 0 && y < SIZES.rows - 1) {
-      if (!grid[id - 1 + SIZES.cols].value && !grid[id - 1 + SIZES.cols].isRevealed) {
-        flipped.push(grid[id - 1 + SIZES.cols]);
-        grid[id - 1 + SIZES.cols].isRevealed = true;
-      }
-    }
-
-    if (x < SIZES.cols - 1 && y > 0) {
-      if (!grid[id + 1 - SIZES.cols].value && !grid[id + 1 - SIZES.cols].isRevealed) {
-        flipped.push(grid[id - 1 + SIZES.cols]);
-        grid[id - 1 + SIZES.cols].isRevealed = true;
-      }
-    }
-
-    if (x < SIZES.cols - 1 && y < SIZES.rows - 1) {
-      if (!grid[id + 1 + SIZES.cols].value && !grid[id + 1 + SIZES.cols].isRevealed) {
-        flipped.push(grid[id + 1 + SIZES.cols]);
-        grid[id + 1 + SIZES.cols].isRevealed = true;
-      }
-    }
-
-    
-
-    // ===================
-
-
-    
-    if (x > 0) {
       if (!grid[id - 1].isRevealed) {
+        grid[id - 1].value
+          ? flipped.unshift(grid[id - 1])
+          : flipped.push(grid[id - 1]);
+
         grid[id - 1].isRevealed = true;
       }
     }
 
-    if (x < SIZES.cols) {
+    if (x < SIZES.cols - 1) {
       if (!grid[id + 1].isRevealed) {
+        grid[id + 1].value
+          ? flipped.unshift(grid[id + 1])
+          : flipped.push(grid[id + 1]);
+
         grid[id + 1].isRevealed = true;
       }
     }
 
     if (y > 0) {
       if (!grid[id - SIZES.cols].isRevealed) {
+        grid[id - SIZES.cols].value
+          ? flipped.unshift(grid[id - SIZES.cols])
+          : flipped.push(grid[id - SIZES.cols]);
+
         grid[id - SIZES.cols].isRevealed = true;
       }
     }
 
-    if (y < SIZES.rows) {
+    if (y < SIZES.rows - 1) {
       if (!grid[id + SIZES.cols].isRevealed) {
+        grid[id + SIZES.cols].value
+          ? flipped.unshift(grid[id + SIZES.cols])
+          : flipped.push(grid[id + SIZES.cols]);
+
         grid[id + SIZES.cols].isRevealed = true;
       }
     }
@@ -106,28 +56,43 @@ export default function revealEmptyCells(grid, id) {
     // diagonals
     if (x > 0 && y > 0) {
       if (!grid[id - 1 - SIZES.cols].isRevealed) {
+        grid[id - 1 - SIZES.cols].value
+          ? flipped.unshift(grid[id - 1 - SIZES.cols])
+          : flipped.push(grid[id - 1 - SIZES.cols]);
+
         grid[id - 1 - SIZES.cols].isRevealed = true;
       }
     }
 
     if (x > 0 && y < SIZES.rows - 1) {
       if (!grid[id - 1 + SIZES.cols].isRevealed) {
+        grid[id - 1 + SIZES.cols].value
+          ? flipped.unshift(grid[id - 1 + SIZES.cols])
+          : flipped.push(grid[id - 1 + SIZES.cols]);
+
         grid[id - 1 + SIZES.cols].isRevealed = true;
       }
     }
 
     if (x < SIZES.cols - 1 && y > 0) {
       if (!grid[id + 1 - SIZES.cols].isRevealed) {
-        grid[id - 1 + SIZES.cols].isRevealed = true;
+        grid[id + 1 - SIZES.cols].value
+          ? flipped.unshift(grid[id + 1 - SIZES.cols])
+          : flipped.push(grid[id + 1 - SIZES.cols]);
+
+        grid[id + 1 - SIZES.cols].isRevealed = true;
       }
     }
 
-    if (y < SIZES.rows - 1) {
+    if (x < SIZES.cols - 1 && y < SIZES.rows - 1) {
       if (!grid[id + 1 + SIZES.cols].isRevealed) {
+        grid[id + 1 + SIZES.cols].value
+          ? flipped.unshift(grid[id + 1 + SIZES.cols])
+          : flipped.push(grid[id + 1 + SIZES.cols]);
+
         grid[id + 1 + SIZES.cols].isRevealed = true;
       }
     }
-
   }
 
   return grid;
